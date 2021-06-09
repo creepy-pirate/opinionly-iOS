1f2ea253ec7c392d71e9b6235dba9998d277fd16
Deepak Singh <dv72276@gmail.com>
2021-06-09 09:22:08 +0530

    The final completed project

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Deepak Singh on 23/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
